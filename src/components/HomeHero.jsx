55af45aa1a2b686ff03a1972d8e6d47db7ee692e
Corey McCue <coreysmccue@gmail.com>
2022-05-22 07:36:28 +0200

    hero image

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -8,8 +8,11 @@ const HomeHero = () => {
 const showText = useMediaQuery(theme.breakpoints.up('md'));
   return (
     <div >
-      { showText &&
-      <ReactPlayer height='100vh' width='100vw' url='https://www.youtube.com/watch?v=YFDGCEui-mk' muted={true} playing={true} />
+      { showText ? (
+      <ReactPlayer height='100vh' width='100vw' loop={true} url='https://www.youtube.com/watch?v=YFDGCEui-mk' muted={true} playing={true} />
+      ) : (
+        <img src="https://www.waitingforsmith.co.uk/uploads/2018/07/WAITING_FOR_SMITH_GS-1680.jpg-lower-res.jpg" alt="waiting for smith" style={{height: '45vh', width: '100vw'}} />
+      )
 }
     </div>
   )
