efce8d20f913ca8bad073eb66b6a2a902877c2f1
Corey McCue <coreysmccue@gmail.com>
2022-06-01 15:06:42 +0200

    add background image to live

--- src/pages/Live.js
+++ src/pages/Live.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 
 import { TourDate } from "../components";
-import Background from "../assets/tour-background.png";
 
 import useContentful from "./content/useContentful";
 
@@ -20,7 +19,7 @@ const Live = () => {
   return (
     <Box
       style={{
-        backgroundImage: `url(${Background})`,
+        backgroundImage: `url(https://scontent-amt2-1.xx.fbcdn.net/v/t39.30808-6/237129931_3009502729262810_1070396645507000933_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a26aad&_nc_ohc=KDDo2rvLEfIAX_rKfB8&tn=XJ6TtWPEEJkapElg&_nc_ht=scontent-amt2-1.xx&oh=00_AT8VGC2Pub18_zAyU-WQdUcoyfHr_EGh-OYYG89Ac3bvxA&oe=629C6700)`,
         backgroundPosition: "sticky",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
