3dc184f905e30b4f88095b4507093855406fc19f
Corey McCue <coreysmccue@gmail.com>
2022-06-02 20:15:06 +0200

    remove mui core

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useTheme, useMediaQuery } from "@material-ui/core";
+import { useTheme, useMediaQuery } from "@mui/material";
 import { Navbar } from "./components";
 
 import {
