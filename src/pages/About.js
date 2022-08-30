8d6fde1af9cd48e03b449df1dc8551b97a2a294e
Corey McCue <coreysmccue@gmail.com>
2022-08-30 22:27:40 +0200

    connect about image from content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import useContentful from "./content/useContentful";
 
-import { Container, Box } from "@mui/material";
+import { Container, Box, Typography } from "@mui/material";
 
 const About = () => {
   const [aboutData, setaboutData] = useState([]);
   const { getAboutPage } = useContentful();
-  console.log("data", aboutData[0])
+  console.log("data", aboutData[0]?.aboutImage?.fields.file?.url)
 
   useEffect(() => {
     getAboutPage().then((data) => setaboutData(data));
@@ -14,9 +14,15 @@ const About = () => {
   }, []);
 
   return (
-    <Container>
-      <Box style={{ marginTop: "8%", fontSize: "3rem" }}>{aboutData[0]?.aboutMain}</Box>
-    </Container>
+    <Container style={{ padding: "none" }}>
+      <Box>
+        {/* <img style={{ height: "100vh", width: "100vw", overflow: "hidden" }} alt="" src={aboutData[0]?.aboutImage?.fields.file?.url} /> */}
+        <Typography style={{
+          marginTop: "8%", fontSize: "2.5rem", color: "white", opacity: "65%", backgroundPositionX: "40%", backgroundPositionY: "20%", backgroundImage: `url(${aboutData[0]?.aboutImage?.fields.file?.url})`
+        }
+        }> {aboutData[0]?.aboutMain}</Typography>
+      </Box>
+    </Container >
   );
 };
 
