ab22871e1d96c0cc65c77d585df933f8cac4e351
Corey McCue <coreysmccue@gmail.com>
2022-08-31 13:56:13 +0200

    connecting tailwindcss to project

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,7 @@ const About = () => {
     <Container style={{ padding: "none" }}>
       <Box>
         {/* <img style={{ height: "100vh", width: "100vw", overflow: "hidden" }} alt="" src={aboutData[0]?.aboutImage?.fields.file?.url} /> */}
-        <Typography style={{
+        <Typography className="text-3xl font-bold" style={{
           marginTop: "8%", fontSize: "2.5rem", color: "white", opacity: "65%", backgroundPositionX: "40%", backgroundPositionY: "20%", backgroundImage: `url(${aboutData[0]?.aboutImage?.fields.file?.url})`
         }
         }> {aboutData[0]?.aboutMain}</Typography>
