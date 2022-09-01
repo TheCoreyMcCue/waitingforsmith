dfe49ea2b6688d844fb23cafb09429a608b36f6f
Corey McCue <coreysmccue@gmail.com>
2022-09-01 19:27:32 +0200

    add about background image

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ import { Container, Box, Typography } from "@mui/material";
 const About = () => {
   const [aboutData, setaboutData] = useState([]);
   const { getAboutPage } = useContentful();
-  
+
   const mobile = window.outerWidth < 400
 
   useEffect(() => {
@@ -16,7 +16,7 @@ const About = () => {
   return (
     <Container disableGutters={true} style={{ maxWidth: '100vw' }}>
       <Box>
-        <img style={{ height: "100vh", width: "100vw", overflow: "hidden", position: "fixed", opacity: "60%" }} alt="" src={aboutData[0]?.aboutImage?.fields.file?.url} />
+        <img style={{ height: "100vh", width: "100vw", overflow: "hidden", position: "fixed", opacity: "60%" }} alt="" src={aboutData[0]?.background?.fields.file?.url} />
         <Typography className="text-3xl font-bold" style={{
           fontSize: "2.5rem", color: "#5e5858", position: "absolute", width: "80vw", height: "80vh", padding: "5%", left: "10%", marginTop: mobile ? "15%" : "1%"
         }
